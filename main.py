92e9735f04fc45ab761fd0e0e5be1e9ef3f80302
killer <michael.molchanov.2004@gmail.com>
2023-12-26 04:46:29 +0100

    using video splitter

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import argparse
+from video_splitter import setup_cli
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Video Processing Application')
+    subparsers = parser.add_subparsers(dest='command')
+    subparsers.required = True
+
+    setup_cli(subparsers)
+
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        args.func(args)
+
+
+if __name__ == '__main__':
+    main()
